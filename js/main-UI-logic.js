6253f63287fe161a23e69afc4a56a6567ff88200
Loisa-Kitakaya <loisakitakaya@gmail.com>
2019-12-18 13:05:20 +0300

    |main_UI-logic.js: scripts|-->|adds functions to control display of recipe details|

--- js/main-UI-logic.js
+++ js/main-UI-logic.js
@@ -24,4 +24,79 @@ $(document).ready(function() {
     $(".recipe-one-details").hide();
     $(".recipe-four-details").slideToggle();
   });
+  //display in grid two
+  $(".recipe-five").click(function() {
+    $(".recipe-six-details").hide();
+    $(".recipe-seven-details").hide();
+    $(".recipe-eight-details").hide();
+    $(".recipe-five-details").slideToggle();
+  });
+  $(".recipe-six").click(function() {
+    $(".recipe-five-details").hide();
+    $(".recipe-seven-details").hide();
+    $(".recipe-eight-details").hide();
+    $(".recipe-six-details").slideToggle();
+  });
+  $(".recipe-seven").click(function() {
+    $(".recipe-six-details").hide();
+    $(".recipe-seven-details").hide();
+    $(".recipe-eight-details").hide();
+    $(".recipe-seven-details").slideToggle();
+  });
+  $(".recipe-four").click(function() {
+    $(".recipe-two-details").hide();
+    $(".recipe-three-details").hide();
+    $(".recipe-one-details").hide();
+    $(".recipe-four-details").slideToggle();
+  });
+  //display in grid two
+  $(".recipe-five").click(function() {
+    $(".recipe-six-details").hide();
+    $(".recipe-seven-details").hide();
+    $(".recipe-eight-details").hide();
+    $(".recipe-five-details").slideToggle();
+  });
+  $(".recipe-six").click(function() {
+    $(".recipe-five-details").hide();
+    $(".recipe-seven-details").hide();
+    $(".recipe-eight-details").hide();
+    $(".recipe-six-details").slideToggle();
+  });
+  $(".recipe-seven").click(function() {
+    $(".recipe-six-details").hide();
+    $(".recipe-five-details").hide();
+    $(".recipe-eight-details").hide();
+    $(".recipe-seven-details").slideToggle();
+  });
+  $(".recipe-eight").click(function() {
+    $(".recipe-six-details").hide();
+    $(".recipe-seven-details").hide();
+    $(".recipe-five-details").hide();
+    $(".recipe-eight-details").slideToggle();
+  });
+  //display grid three
+  $(".recipe-nine").click(function() {
+    $(".recipe-ten-details").hide();
+    $(".recipe-eleven-details").hide();
+    $(".recipe-twelve-details").hide();
+    $(".recipe-nine-details").slideToggle();
+  });
+  $(".recipe-ten").click(function() {
+    $(".recipe-nine-details").hide();
+    $(".recipe-eleven-details").hide();
+    $(".recipe-twelve-details").hide();
+    $(".recipe-ten-details").slideToggle();
+  });
+  $(".recipe-eleven").click(function() {
+    $(".recipe-ten-details").hide();
+    $(".recipe-nine-details").hide();
+    $(".recipe-twelve-details").hide();
+    $(".recipe-eleven-details").slideToggle();
+  });
+  $(".recipe-twelve").click(function() {
+    $(".recipe-ten-details").hide();
+    $(".recipe-eleven-details").hide();
+    $(".recipe-nine-details").hide();
+    $(".recipe-twelve-details").slideToggle();
+  });
 });

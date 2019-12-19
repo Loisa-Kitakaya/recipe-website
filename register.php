b1e5436b27ea780f802eed2f6f1b22bd34e7870a
Andrew-1000 <andruambia95@gmail.com>
2019-12-19 09:04:07 +0300

    Update the register.php file to change the form action to reg-action.php

--- register.php
+++ register.php
@@ -4,13 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="./css/login-styles.css">
   <title>Document</title>
 </head>
 <body>
 <div class = "container" >
 	        <img src="images/set.png"/>
 	        <p id ="para1">Please provide your details below to login</p>
-	<form name="formreg" action="action.php" id="formreg" method="post" >
+	<form name="formreg" action="reg-action.php" id="formreg" method="post" >
 
 			<div class = "form-input">
 			<input type="text" name = "firstname" placeholder = "First Name">

d57bf00352c86bcf73a37859e41553e6bac0ed11
Andrew-1000 <andruambia95@gmail.com>
2019-12-19 09:00:45 +0300

    Add a new file reg-action.php to execute instructions when one tries to register for an account

---
+++ reg-action.php
@@ -0,0 +1,29 @@
+
+<!-- Code for allowing a user to register for an account -->
+<?php
+
+$conn = mysqli_connect('localhost', 'root', '','records');
+if (!$conn)
+{
+	echo 'Error '.mysqli_error($conn);
+}
+//Process
+if (isset($_POST['submit']))
+{
+
+$firstname = addslashes( $_POST['firstname'] ); //prevents types of SQL injection
+$lastname = addslashes( $_POST['lastname'] ); //prevents types of SQL injection
+$contact = addslashes( $_POST['contact'] ); //prevents type
+$email = addslashes( $_POST['emailadd'] );
+$username = $_POST['username'];
+$password = $_POST['password'];
+$newpass = md5($password); //This will make your password encrypted into md5, a high security hash
+
+$sql = mysqli_query( $conn,"INSERT INTO users(fname, lname, contact, email, username, hash) VALUES ('$firstname','$lastname', '$contact', '$email', '$username', '$newpass')" )
+        or die( mysqli_error($conn) );
+
+die( "You have registered for an account.<br><br>Go to <a href=\"index.php\">Login</a>");
+}
+
+ob_end_flush();
+?>
---
+++ register.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+<div class = "container" >
+	        <img src="images/set.png"/>
+	        <p id ="para1">Please provide your details below to login</p>
+	<form name="formreg" action="action.php" id="formreg" method="post" >
+
+			<div class = "form-input">
+			<input type="text" name = "firstname" placeholder = "First Name">
+	        </div>
+			<div class = "form-input">
+			<input type="text" name = "lastname" placeholder = "Last Name">
+	        </div>
+			<div class = "form-input">
+			<input type="text" name = "contact" placeholder = "Contact">
+	        </div>
+			<div class = "form-input">
+			<input type="text" name = "emailadd" placeholder = "Valid Email Address">
+	        </div>
+			<div class = "form-input">
+			<input type="text" name = "username" placeholder = "Enter Username">
+	        </div>
+
+	        <div class="form-input">
+	        <input type="password" name="password" placeholder = "Enter Password">
+
+	        </div>
+			<div class="form-input">
+	        <input type="password" name="confirmpassword" placeholder = "Reenter Password">
+
+	        </div>
+
+	        <input type = "submit" name = "submit" value ="REGISTER" class="btn-login">
+
+
+
+	</form>
+</body>
+</html>
